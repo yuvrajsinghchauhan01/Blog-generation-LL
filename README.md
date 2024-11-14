8e0a892a61569b02707bf4433f6e3e54efd4a989
yuvrajsinghchauhan <ychauhan191@gmail.com>
2024-11-14 14:21:47 +0530

    updated

---
+++ README.md
@@ -0,0 +1,5 @@
+## How to Run the application
+1. download the `TheBloke/Llama-2-7B-Chat-GGML` model from `hugging face`.
+2. save it in `model` directory
+3. install the packages from `requirements.txt`
+4. Enjoy...
---
+++ app.py
@@ -0,0 +1,56 @@
+import streamlit as st
+from langchain.prompts import PromptTemplate
+from langchain_community.llms import CTransformers
+
+## Function To get response from Llama 2 model
+
+def getLLamaresponse(input_text, no_words, blog_style):
+    try:
+        ## Ensure no_words is an integer
+        no_words = int(no_words)
+    except ValueError:
+        return "Please enter a valid number for 'No of Words'"
+
+    ## Llama 2 model
+    llm = CTransformers(
+        model='model/llama-2-7b-chat.ggmlv3.q8_0.bin',
+        model_type='llama',
+        config={'max_new_tokens': 256, 'temperature': 0.01}
+    )
+    
+    ## Prompt Template
+    template = """
+        Write a blog for {blog_style} job profile for a topic {input_text}
+        within {no_words} words.
+    """
+    
+    prompt = PromptTemplate(input_variables=["blog_style", "input_text", 'no_words'], template=template)
+    
+    ## Generate the response from the Llama 2 model
+    response = llm(prompt.format(blog_style=blog_style, input_text=input_text, no_words=no_words))
+    print(response)
+    return response
+
+
+st.set_page_config(page_title="Generate Blogs", page_icon='🤖', layout='centered', initial_sidebar_state='collapsed')
+
+st.header("Generate Blogs 🤖")
+
+input_text = st.text_input("Enter the Blog Topic")
+
+## Creating two more columns for additional 2 fields
+col1, col2 = st.columns([5, 5])
+
+with col1:
+    no_words = st.text_input('No of Words')
+with col2:
+    blog_style = st.selectbox('Writing the blog for', ('Researchers', 'Data Scientist', 'Common People'), index=0)
+
+submit = st.button("Generate")
+
+## Final response
+if submit:
+    if input_text:  # Check if the input_text is not empty
+        st.write(getLLamaresponse(input_text, no_words, blog_style))
+    else:
+        st.write("Please enter a blog topic.")
